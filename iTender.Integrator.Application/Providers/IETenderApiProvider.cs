96e76be9b1157b9bd9edfa7cafe191646e6c76d8
Teboho Mosakoa <TebohoM@cidb.org.za>
2026-09-08 09:03:05 +0200

    08 September 2026

---
+++ iTender.Integrator.Application/Providers/IETenderApiProvider.cs
@@ -0,0 +1,9 @@
+﻿using iTender.Integrator.Application.DTOs.Etender;
+
+namespace iTender.Integrator.Application.Providers
+{
+    public interface IETenderApiProvider
+    {
+        Task<List<ExternalTenderModel>> GetTendersAsync(CancellationToken ct = default);
+    }
+}
