d43c0f2f3067aa755d05578b384904d27b039c5f
Teboho Mosakoa <TebohoM@cidb.org.za>
2026-09-15 14:54:18 +0200

    Tuesdays - 15 September 2026 - Separate iTender Tender Status

--- iTender.Integrator.Domain/Enums/TenderStatus.cs
+++ iTender.Integrator.Domain/Enums/TenderStatus.cs
@@ -3,9 +3,11 @@
     public enum TenderStatus
     {
         Unknown = 0,
-        DRAFT_STATUS = 1,
-        ADVERTISED_STATUS = 100000000,
-        CANCELLED_STATUS = 100000001,
-        CLOSED_STATUS = 100000002
+        Planning,
+        Active,
+        Cancelled,
+        Unsuccessful,
+        Complete,
+        Withdrawn
     }
 }
---
+++ iTender.Integrator.Domain/Enums/iTenderTenderStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace iTender.Integrator.Domain.Enums
+{
+    public enum iTenderTenderStatus
+    {
+        Unknown = 0,
+        DRAFT_STATUS = 1,
+        ADVERTISED_STATUS = 100000000,
+        CANCELLED_STATUS = 100000001,
+        CLOSED_STATUS = 100000002
+    }
+}
